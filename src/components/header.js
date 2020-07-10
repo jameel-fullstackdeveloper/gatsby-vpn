b48994147e31ccf6b994f21743039296a41c36cd
jameelfullstackdev <jameel@jameelahmed.com.pk>
2020-07-10 18:33:53 +0500

    home page udates

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = () => {
     <header  id="slider-area">  
        <nav className="navbar navbar-expand-md scrolling-navbar bg-dark">
         <div className="container-fluid">          
-          <Link className="navbar-brand" to="index.html">
+          <Link className="navbar-brand" to="/">
                 <img src={logo} className="img-fluid" alt="Logo" />
           </Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,16 +18,16 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto w-100 justify-content-end">
               <li className="nav-item">
-                <Link className="nav-link" to="features.html">Features</Link>
+                <Link className="nav-link" to="features">Features</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="price.html">Price</Link>
+                <Link className="nav-link" to="price">Price</Link>
               </li>  
               <li className="nav-item">
-                <Link className="nav-link" to="vpnapps.html">VPN Apps</Link>
+                <Link className="nav-link" to="vpnapps">VPN Apps</Link>
               </li>                            
               <li className="nav-item">
-                <Link className="nav-link" to="help.html">Help</Link>
+                <Link className="nav-link" to="help">Help</Link>
               </li>            
             </ul>              
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,43 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import banner from '../images/banner.png'
+import unblockwebsite from '../images/unblock-website.png'
+
 
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div id="carousel-area">
+        <div id="carousel-slider" className="carousel slide carousel-fade" data-ride="carousel">
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active">
+              <img src={banner} className="img-fluid" alt="" />
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      <section id="unblock-websites">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 order-md-1">
+                    <img src={unblockwebsite} className="img-fluid" alt="" />
+                </div>
+                <div className="col-md-6 order-md-2 pl-5">
+                    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+                    <h1>Unblock Websites</h1>
+                    <p>
+                        RitaVPN helps you to unblock websites in your network from around the globe. Provides unrestricted access to the web by allowing you to unblock websites. Get access to your favorite websites and content like Facebook, Twitter, and Pinterest that be blocked in your country from anywhere and anytime.
+                    </p>
+                    <p>&nbsp;</p>
+                    <a href="http://localhost:8000" className="btn btn-primary">Get RitaVPN</a>
+                </div>
+            </div>
+        </div>  
+    </section>
+
     
   </Layout>
 )
